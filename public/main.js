5e16e6b242b0d61e1e80f1c7e0a9fb4fbab16c82
Mateus <dutra22ms@gmail.com>
2021-03-27 23:17:10 -0300

    Tá quase no começo da onde eu quero ir

--- public/main.js
+++ public/main.js
@@ -1,7 +1,50 @@
-function add(){
-    let element = document.getElementById('content')
-    let first = element.children[0];
-    let novo = React.createElement('p', null, "plus one")
-    let board = React.createElement('div', null, ...first.children, novo);
-    ReactDOM.render(board, element);
+function eraseAll(){
+    sessionStorage.clear();
 }
+
+function add(){
+    let actual = Number(sessionStorage.getItem('clicks'));
+    sessionStorage.setItem('clicks', actual + 1);
+    console.log(sessionStorage.getItem('clicks'));
+}
+
+function addOne(){
+    console.log(this)
+}
+
+function Content({qtd}){
+        let cont = []
+        for(let i = 0; i < qtd; i++){
+            cont.push(<p key={i}>Plus one</p>)
+        }
+        return(
+            <>
+            {cont}
+            </>
+        )
+        
+}
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {qtd:props.qtd};
+        addOne.bind(this.state);
+    }
+
+    render(){
+        return(
+        <>
+            <Content qtd = {this.state.qtd}/>
+        </>
+        )
+    }
+}
+sessionStorage.setItem('clicks', 2);
+let clicks = sessionStorage.getItem("clicks");
+console.log(clicks);
+addOne.bind
+ReactDOM.render(
+    <Board qtd={clicks}/>,
+    document.getElementById('content')
+)
