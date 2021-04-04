fbfa8ccd20808ac7d54b7297e6ba9252248994f8
Mateus <dutra22ms@gmail.com>
2021-04-04 03:27:55 -0300

    Tá indo

--- public/main.js
+++ public/main.js
@@ -14,40 +14,80 @@ class Row extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            title :'Título',
+            title :'Sem Título',
             content:'',
             expandedDisplay:'none',
-            key:props.index
+            columnIndex:props.column,
+            rowIndex:props.row,
+            expandedStyle : {
+                left: 0.2 * window.innerWidth,
+                top: 0.1 * window.innerHeight,
+                width: 0.6 * window.innerWidth,
+                height: 0.8 * window.innerHeight
+            }
         }
         console.log(window.innerHeight, window.innerWidth)
         this.expand = this.expand.bind(this);
+        this.adapt = this.adapt.bind(this);
+        window.addEventListener('resize', this.adapt);
     }
 
     expand(){
+        if(this.state.expandedDisplay == 'block'){
+            this.storeChange();
+        }
         this.setState(prevState =>({
             expandedDisplay:(prevState.expandedDisplay == 'none'? 'block':'none')
         }));
     }
 
+    adapt(){
+        this.setState(() =>({
+            expandedStyle: {
+                left: 0.2 * window.innerWidth,
+                top: 0.1 * window.innerHeight,
+                width: 0.6 * window.innerWidth,
+                height: 0.8 * window.innerHeight,
+            }
+        }));
+    }
+
+    storeChange(){
+        let e = document
+        .getElementsByClassName("columns")[this.state.columnIndex]
+        .getElementsByClassName("Rows")[0]
+        .getElementsByClassName("Row")[this.state.rowIndex]
+        .getElementsByClassName("expand")[0];
+        let t = e.getElementsByClassName("title")[0].value;
+        if(!t){
+            t = "Título";
+        }
+        let d = e.getElementsByClassName("description")[0].value;
+        this.setState(()=>({
+            title:t,
+            content:d
+        }));
+    }
+
     render(){
-        let expandedStyle = {
-            left: (window.innerWidth/2) - 320,
-            top: (window.innerHeight/2) - 200,
+        let title = (this.state.title?this.state.title:"Sem Título");
+        let fullExpandedStyle = {
+            ...this.state.expandedStyle,
             display: this.state.expandedDisplay
         }
 
-        let overlayStyle = {
-            display:this.state.expandedDisplay, 
-            height:window.innerHeight, 
-            width:window.innerWidth
+        let fullOverlayStyle = {
+            display: this.state.expandedDisplay
         }
         return(
             <div className="Row" onDoubleClick={this.expand}>
-                <p>{this.state.title}</p>
-                <div className="overlayDisplay" onClick={this.expand} style={overlayStyle}></div>
-                    <div className="expand" style={expandedStyle}>
-                        <input type='text'className='title'></input>
-                        <input type='text' className="description"></input>
+                <p>{title}</p>
+                <div className="overlayDisplay" onClick={this.expand} style={fullOverlayStyle}></div>
+                <div className="expand" style={fullExpandedStyle}>
+                    <label htmlFor='title' className='expandLabel'><b>Título</b></label>
+                    <input type='text' name='title' className='title' placeholder="Insira um título"></input>
+                    <label htmlFor='description' className='expandLabel'><b>Descrição</b></label>
+                    <textarea name='description' className="description" placeholder='Insira uma descrição' rows="" cols=""></textarea>
                 </div>
             </div>
         )
@@ -58,12 +98,11 @@ class Column extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            qtd : 1
+            qtd:1,
+            columnIndex: props.column
         };
         this.add = this.add.bind(this);
     }
-
-    
 
     add(){
         this.setState(prevState => ({
@@ -71,14 +110,15 @@ class Column extends React.Component{
         }));
     }
 
+
     render(){
         let rows = [];
         for(let i = 0; i < this.state.qtd; i++){
-            rows.push(<Row key={i} index={i}/>);
+            rows.push(<Row key={i} row={i} column={this.state.columnIndex} index={i}/>);
         }
         return(
         <>
-            <input type="text" placeholder="Título"></input>
+            <input type="text" className="columnTitle" placeholder="Título"></input>
             <div className="Rows">
                 {rows}
             </div>
@@ -95,8 +135,7 @@ class Board extends React.Component{
             clicks:2, 
             hide: true,
             contentStyle:{
-                height: window.innerHeight - 30,
-                
+                height: window.innerHeight - 30
             }
         };
         this.add = this.add.bind(this);
@@ -107,13 +146,13 @@ class Board extends React.Component{
     add(){
         this.setState(prevState => ({
             clicks: prevState.clicks + 1
-          }));
+        }));
     }
 
     hide(){
         this.setState(prevState => ({
             hide: !prevState.hide
-          }));
+        }));
     }
 
     render(){
@@ -131,7 +170,7 @@ class Board extends React.Component{
             <>
             <div id='content' style={this.state.contentStyle}>
                 <div className='columns'>
-                    <Column/>
+                    <Column column = {0}/>
                 </div>
                 
             </div>
